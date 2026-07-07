bc5486c3c0efccec0ba4a65805bb75fea91a6fdd
George Purves <georgetaylor2104@gmail.com>
2026-07-06 21:36:12 -0600

    ChessGame constructor fixed

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * A class that can manage a chess game, making moves on a board
@@ -11,10 +12,10 @@ import java.util.Collection;
 public class ChessGame {
 
     TeamColor teamTurn = TeamColor.WHITE;
-    ChessBoard gameBoard;
+    ChessBoard gameBoard = new ChessBoard();
 
     public ChessGame() {
-
+        gameBoard.resetBoard();
     }
 
     /**
@@ -100,6 +101,7 @@ public class ChessGame {
      */
     public void setBoard(ChessBoard board) {
         gameBoard = board;
+
     }
 
     /**
@@ -109,5 +111,19 @@ public class ChessGame {
      */
     public ChessBoard getBoard() {
         return gameBoard;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessGame chessGame = (ChessGame) o;
+        return getTeamTurn() == chessGame.getTeamTurn() && Objects.equals(gameBoard, chessGame.gameBoard);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTeamTurn(), gameBoard);
     }
 }
